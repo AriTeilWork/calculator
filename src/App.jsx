2b53b1e3a26b43832ac78543bcf5397b2cf2b8d3
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-26 22:10:50 +0200

    Implement Calculation Logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import InputComponent from './components/Input';
 import OperatorComponent from './components/Operator';
 import ResultComponent from './components/Result';
 
-const calculateResult = () => {
-  if (operator === '+') setResult(num1 + num2);
-  else if (operator === '-') setResult(num1 - num2);
-  else if (operator === '*') setResult(num1 * num2);
-  else if (operator === '/') setResult(num2 !== 0 ? num1 / num2 : 'Error');
-};
-
-
 const App = () => {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   const [operator, setOperator] = useState(null);
   const [result, setResult] = useState(null);
+
+  const calculateResult = () => {
+    if (operator === '+') setResult(num1 + num2);
+    else if (operator === '-') setResult(num1 - num2);
+    else if (operator === '*') setResult(num1 * num2);
+    else if (operator === '/') setResult(num2 !== 0 ? num1 / num2 : 'Error');
+  };
 
   return (
     <div className="p-4 space-y-4">
@@ -23,7 +22,12 @@ const App = () => {
         <InputComponent value={num1} onChange={setNum1} />
         <InputComponent value={num2} onChange={setNum2} />
       </div>
-      <OperatorComponent onSelect={(op) => { setOperator(op); calculateResult(); }} />
+      <OperatorComponent
+        onSelect={(op) => {
+          setOperator(op);
+          calculateResult();
+        }}
+      />
       <ResultComponent result={result} />
     </div>
   );
