917f5f644f0da97891035d801a2c9631b06a2fd3
AriTeil <118675580+AriTeilWork@users.noreply.github.com>
2025-03-26 22:00:42 +0200

    A component for selecting mathematical operators with styled buttons

--- src/components/Operator.jsx
+++ src/components/Operator.jsx
@@ -1,2 +1,19 @@
+const OperatorComponent = ({ onSelect }) => {
+  const operators = ['+', '-', '*', '/'];
 
+  return (
+    <div className="flex space-x-2">
+      {operators.map((operator) => (
+        <button
+          key={operator}
+          onClick={() => onSelect(operator)}
+          className="border p-2 rounded-md bg-blue-500 text-white"
+        >
+          {operator}
+        </button>
+      ))}
+    </div>
+  );
+};
 
+export default OperatorComponent;
